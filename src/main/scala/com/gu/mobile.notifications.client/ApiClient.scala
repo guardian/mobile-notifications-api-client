0c1b15d74c2a2df7adba8c84f75f7e53c76a1b87
pvighi <patricio.vighi@guardian.co.uk>
2015-11-24 12:31:37 +0000

    MAPI-1070
    refactored, added client id to composite errors and added tests

--- src/main/scala/com/gu/mobile.notifications.client/ApiClient.scala
+++ src/main/scala/com/gu/mobile.notifications.client/ApiClient.scala
@@ -9,29 +9,45 @@ import scala.concurrent.{ExecutionContext, Future}
 
 
 sealed trait ApiClientError {
-  def message: String
+  def description: String
 }
 
-trait CompositeApiError extends ApiClientError{
-  def errors : List[ApiClientError]
-  override def message: String = errors.map(_.message).mkString("\n")
-}
-case class PartialApiError(errors: List[ApiClientError]) extends CompositeApiError
-case class TotalApiError(errors: List[ApiClientError]) extends CompositeApiError
-case class HttpApiError(status:Int) extends ApiClientError {
-  val message = s"Http error status $status"
+case class ErrorWithSource(clientId:String, error:ApiClientError){
+  def description = clientId + ": " + error.description
 }
 
+trait CompositeApiError extends ApiClientError {
+  def errors: List[ErrorWithSource]
+  override def description: String = errors.map(e=>e.description).mkString(", ")
+}
 
+case class PartialApiError(errors: List[ErrorWithSource]) extends CompositeApiError
+
+case class TotalApiError(errors: List[ErrorWithSource]) extends CompositeApiError
+
+case class HttpApiError( status: Int) extends ApiClientError {
+  val description = s"Http error status $status"
+}
 
 trait ApiClient {
+  //used to identify the client on error reports
+  def clientId:String
   def send(notificationPayload: NotificationPayload)(implicit ec: ExecutionContext): Future[Either[ApiClientError, String]]
 }
 
 trait SimpleHttpApiClient extends ApiClient {
   def host: String
+
   def httpProvider: HttpProvider
-  def apiKey: String
+
+  def apiKey: Option[String]
+
+  protected val url = s"$host/notifications" + apiKeyPathPart
+
+  private def apiKeyPathPart = apiKey match {
+    case None => ""
+    case Some(key) => s"?api-key=$key"
+  }
 
   def healthcheck(implicit ec: ExecutionContext): Future[Healthcheck] = {
     httpProvider.get(s"$host/healthcheck").map {
@@ -40,57 +56,80 @@ trait SimpleHttpApiClient extends ApiClient {
       case HttpError(code, _) => Unhealthy(Some(code))
     }
   }
+
+  protected def postJson(json: String) = {
+    httpProvider.post(
+      url = this.url,
+      contentType = ContentType("application/json", "UTF-8"),
+      body = json.getBytes("UTF-8")
+    )
+  }
 }
 
 class LegacyApiClient(val host: String,
                       val httpProvider: HttpProvider,
-                      val apiKey: String,
+                      val apiKey: Option[String] = None,
+                      val clientId:String = "Legacy",
                       payloadBuilder: PayloadBuilder = PayloadBuilderImpl) extends SimpleHttpApiClient {
 
-
   override def send(notificationPayload: NotificationPayload)(implicit ec: ExecutionContext): Future[Either[ApiClientError, String]] = {
-
-      val notification = payloadBuilder.buildNotification(notificationPayload)
-      this.send(notification) map (x => Right(x.messageId)) recover {case error: HttpError => Left(HttpApiError(error.status))}
+    val legacyNotification = payloadBuilder.buildNotification(notificationPayload)
+    sendToServer(legacyNotification) map {
+      case Left(httpError) => Left(HttpApiError(httpError.status))
+      case Right(sendNotificationReply) => Right(sendNotificationReply.messageId)
+    }
   }
 
   def send(notification: Notification)(implicit ec: ExecutionContext): Future[SendNotificationReply] = {
+    sendToServer(notification) map {
+      case Right(sendNotificationReply) => sendNotificationReply
+      case Left(error) => throw error
+    }
+  }
+
+  private def sendToServer(notification: Notification)(implicit ec: ExecutionContext): Future[Either[HttpError, SendNotificationReply]] = {
     val json = Json.stringify(Json.toJson(notification))
-    httpProvider.post(
-      url = if (apiKey.isEmpty) s"$host/notifications" else s"$host/notifications?api-key=$apiKey",
-      contentType = ContentType("application/json", "UTF-8"),
-      body = json.getBytes("UTF-8")
-    ) map {
-      case HttpOk(code, body) => Json.fromJson[SendNotificationReply](Json.parse(body)).get
-      case error: HttpError => throw error
+    postJson(json) map {
+      case HttpOk(code, body) => Right(Json.fromJson[SendNotificationReply](Json.parse(body)).get)
+      case error: HttpError => Left(error)
     }
   }
 }
 
 class N10nApiClient(val host: String,
                     val httpProvider: HttpProvider,
-                    val apiKey: String) extends SimpleHttpApiClient {
-  //TODO SEE EXACTLY WHAT TO POST HERE AND WHERE AND HOW TO PARSE THE RESULT
-  override def send(notificationPayload: NotificationPayload)(implicit ec: ExecutionContext): Future[Either[ApiClientError, String]] = throw new UnsupportedOperationException("Method not implemented")
-
+                    val clientId:String = "n10n",
+                    val apiKey: Option[String]) extends SimpleHttpApiClient {
+  override def send(notificationPayload: NotificationPayload)(implicit ec: ExecutionContext): Future[Either[ApiClientError, String]] = {
+    //TODO
+   // val json = Json.stringify(Json.toJson(notificationPayload))
+    Future(Left(HttpApiError(500)))
+  }
 }
 
 //TODO tag the api clients so that we can return a meaningful error code that can be displayed to the user
-class CompositeApiClient(apiClients: List[ApiClient]) extends ApiClient {
+class CompositeApiClient(apiClients: List[ApiClient], val clientId:String = "composite") extends ApiClient {
   require(apiClients.size > 0)
 
   override def send(notificationPayload: NotificationPayload)(implicit ec: ExecutionContext): Future[Either[ApiClientError, String]] = {
-    def sendWithRetry(apiClient: ApiClient) = apiClient.send(notificationPayload)
 
-    val responses = Future.sequence(apiClients.map(sendWithRetry))//TODO make sure there are no future failures here
+    def sendAndSource(apiClient: ApiClient) : Future[Either[ErrorWithSource, String]] = {
+      apiClient.send(notificationPayload) map {
+        case Left(error) => Left(ErrorWithSource(apiClient.clientId, error))
+        case Right(x) => Right(x)
+      }
+    }
+
+    val responses = Future.sequence(apiClients.map(sendAndSource)) //TODO make sure there are no future failures here (but how?)
     responses.map(aggregateResponses)
   }
 
-  //from the responses to the individual call figure out what response to give to the composite api user
-  private def aggregateResponses(responses: List[Either[ApiClientError, String]]): Either[ApiClientError, String] = {
+  //from the responses to the individual calls figure out what response to give to the composite api user
+  private def aggregateResponses(responses: List[Either[ErrorWithSource, String]]): Either[ApiClientError, String] = {
     responses.partition(_.isLeft) match {
-      case (Nil, rights) => rights(0)
-      case (lefts, _) => Left(TotalApiError(lefts.map(_.left.get)))
+      case (Nil, Right(res) :: _) => Right(res)
+      case (lefts, Nil) => Left(TotalApiError(lefts.map(_.left.get)))
+      case (lefts, _) => Left(PartialApiError(lefts.map(_.left.get)))
 
     }
   }
--- src/test/scala/com/gu/mobile/notifications/client/ApiClientSpec.scala
+++ src/test/scala/com/gu/mobile/notifications/client/ApiClientSpec.scala
@@ -2,10 +2,10 @@ package com.gu.mobile.notifications.client
 
 import com.gu.mobile.notifications.client.legacy.PayloadBuilder
 import com.gu.mobile.notifications.client.models.NotificationTypes.BreakingNews
-import com.gu.mobile.notifications.client.models.Priority.Major
 import com.gu.mobile.notifications.client.models.Regions.UK
 import com.gu.mobile.notifications.client.models._
 import com.gu.mobile.notifications.client.models.legacy._
+import org.specs2.execute.Result
 import org.specs2.mock.Mockito
 import org.specs2.mock.mockito.ArgumentCapture
 import org.specs2.mutable.Specification
@@ -16,7 +16,8 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class ApiClientSpec extends Specification with Mockito with NoTimeConversions {
   val legacyHost = "http://legacyHost.co.uk"
-  val apiKey="apiKey"
+  val apiKey = "apiKey"
+
   val iosPayload = IOSMessagePayload(
     body = "ios_body",
     customProperties = Map("p1" -> "v1"),
@@ -36,121 +37,118 @@ class ApiClientSpec extends Specification with Mockito with NoTimeConversions {
     metadata = Map("m1" -> "v1")
   )
 
-  //the values in this payload object don't matter as the payloadBuilder used to convert it to a notification object is faked anyway...
-  val payload = BreakingNewsPayload(
-    title = "myTile",
-    message = "msg",
-    thumbnailUrl = None,
-    sender = "sender",
-    editions = Set.empty,
-    link = ExternalLink("url"),
-    imageUrl = None,
-    priority = Major,
-    topic = Set.empty,
-    debug = false)
-
+  val payload = mock[BreakingNewsPayload]
   val fakePayloadBuilder = mock[PayloadBuilder]
   fakePayloadBuilder.buildNotification(payload) returns notification
 
+  def legacyApiTest(test: LegacyApiClient => Unit):Result = {
+    val successServerResponse = HttpOk(200, "{\"messageId\":\"123\"}")
+    legacyApiTest(successServerResponse)(test)
+  }
 
-  //TODO I WOULD PREFER TO MOCK OUT THE JSON GENERATION AND TEST THAT ON ITS OWN TEST
+  def legacyApiTest(serverResponse:HttpResponse)(test: LegacyApiClient => Unit):Result = {
+    val fakeHttpProvider = mock[HttpProvider]
+    fakeHttpProvider.post(anyString, any[ContentType], any[Array[Byte]]) returns Future.successful(serverResponse)
+
+    val serviceApi = new LegacyApiClient(
+      apiKey = Some(apiKey),
+      httpProvider = fakeHttpProvider,
+      host = legacyHost,
+      payloadBuilder = fakePayloadBuilder
+    )
+    test(serviceApi)
+
+    val bodyCapture = new ArgumentCapture[Array[Byte]]
+    val urlCapture = new ArgumentCapture[String]
+    val contentTypeCapture = new ArgumentCapture[ContentType]
+
+    there was one(fakeHttpProvider).post(urlCapture, contentTypeCapture, bodyCapture)
+    urlCapture.value mustEqual (s"$legacyHost/notifications?api-key=$apiKey")
+    contentTypeCapture.value mustEqual (ContentType("application/json", "UTF-8"))
+    new String(bodyCapture.value) mustEqual (notificationAsJson)
+  }
+
   val notificationAsJson = """{"type":"news","uniqueIdentifier":"UNIQUE_ID","sender":"sender","target":{"regions":["uk"],"topics":[{"type":"newsstand","name":"newsstandIos"}]},"timeToLiveInSeconds":10,"payloads":{"ios":{"type":"ios","body":"ios_body","customProperties":{"p1":"v1"},"category":"category"},"android":{"type":"android","body":{"k1":"v1"}}},"metadata":{"m1":"v1"}}"""
 
-
+//TODO WE NEED TO COVER OTHER API ERRORS
   "LegacyApiClient" should {
 
-    //TODO EXTRACT DUPLICATED CODE INTO SOME COMMON PLACE
-    "successfully send notification" in {
-
-      val fakeHttpProvider = mock[HttpProvider]
-      fakeHttpProvider.post(anyString, any[ContentType], any[Array[Byte]]) returns Future.successful(HttpOk(200, "{\"messageId\":\"123\"}"))
-
-      val serviceApi = new LegacyApiClient (
-        apiKey = apiKey,
-        httpProvider = fakeHttpProvider,
-        host = legacyHost
-      )
-
-      val reply = serviceApi.send(notification)
-      reply must beEqualTo(SendNotificationReply("123")).await
-      val bodyCapture = new ArgumentCapture[Array[Byte]]
-      val urlCapture = new ArgumentCapture[String]
-      val contentTypeCapture = new ArgumentCapture[ContentType]
-
-      there was one(fakeHttpProvider).post(urlCapture, contentTypeCapture, bodyCapture)
-      urlCapture.value mustEqual (s"$legacyHost/notifications?api-key=$apiKey")
-      contentTypeCapture.value mustEqual (ContentType("application/json", "UTF-8"))
-      new String(bodyCapture.value) mustEqual (notificationAsJson)
+    "successfully send legacacy notification object" in legacyApiTest {
+      legacyApiClient => legacyApiClient.send(notification) must beEqualTo(SendNotificationReply("123")).await
     }
-
-    "successfully send BreakingNewsPayload" in {
-
-      val fakeHttpProvider = mock[HttpProvider]
-      fakeHttpProvider.post(anyString, any[ContentType], any[Array[Byte]]) returns Future.successful(HttpOk(200, "{\"messageId\":\"123\"}"))
-
-      val serviceApi = new LegacyApiClient (
-        apiKey = apiKey,
-        httpProvider = fakeHttpProvider,
-        host = legacyHost,
-        payloadBuilder = fakePayloadBuilder
-      )
-
-      val reply = serviceApi.send(payload)
-      reply must beEqualTo(Right("123")).await
-      val bodyCapture = new ArgumentCapture[Array[Byte]]
-      val urlCapture = new ArgumentCapture[String]
-      val contentTypeCapture = new ArgumentCapture[ContentType]
-
-      there was one(fakeHttpProvider).post(urlCapture, contentTypeCapture, bodyCapture)
-      urlCapture.value mustEqual (s"$legacyHost/notifications?api-key=$apiKey")
-      contentTypeCapture.value mustEqual (ContentType("application/json", "UTF-8"))
-      new String(bodyCapture.value) mustEqual (notificationAsJson)
+    "successfully send BreakingNewsPayload" in legacyApiTest {
+      legacyApiClient => legacyApiClient.send(payload) must beEqualTo(Right("123")).await
     }
-
-    "return error if cannot send BreakingNewsPayload" in {
-
-      val fakeHttpProvider = mock[HttpProvider]
-      //TODO looks like when running this test is just throws this error here for some reason instead of just creating the instance
-      val httpError = HttpError(500, "")
-      fakeHttpProvider.post(anyString, any[ContentType], any[Array[Byte]]) returns Future.successful(httpError)
-
-      val serviceApi = new LegacyApiClient (
-        apiKey = apiKey,
-        httpProvider = fakeHttpProvider,
-        host = legacyHost,
-        payloadBuilder = fakePayloadBuilder
-      )
-
-      val reply = serviceApi.send(payload)
-      reply must beEqualTo(Left(HttpApiError(status = 500))).await
-      val bodyCapture = new ArgumentCapture[Array[Byte]]
-      val urlCapture = new ArgumentCapture[String]
-      val contentTypeCapture = new ArgumentCapture[ContentType]
-
-      there was one(fakeHttpProvider).post(urlCapture, contentTypeCapture, bodyCapture)
-      urlCapture.value mustEqual (s"$legacyHost/notifications?api-key=$apiKey")
-      contentTypeCapture.value mustEqual (ContentType("application/json", "UTF-8"))
-      new String(bodyCapture.value) mustEqual (notificationAsJson)
-
+    "return error if cannot send BreakingNewsPayload" in legacyApiTest(serverResponse = HttpError(500, "")) {
+      legacyApiClient => legacyApiClient.send(payload) must beEqualTo(Left(HttpApiError(status = 500))).await
     }
-
+    "throw error if cannot send legacy notification object" in legacyApiTest(serverResponse = HttpError(500, "")) {
+      legacyApiClient => legacyApiClient.send(notification) must throwA[HttpError].await
+    }
   }
+
   "CompositeApiClient" should {
-    "Collect errors" in {
+    "Report total error if all api calls fail" in {
+      val api1 = mock[ApiClient]
+      val api2 = mock[ApiClient]
+      val api3 = mock[ApiClient]
+
       val error1 = HttpApiError(500)
       val error2 = HttpApiError(400)
-      val api1 = mock[ApiClient]
-      api1.send(any[NotificationPayload])(any[ExecutionContext])returns Future(Left(error1))
-      val api2 = mock[ApiClient]
-      api2.send(any[NotificationPayload])(any[ExecutionContext])returns Future(Left(error2))
+      val error3 = HttpApiError(403)
 
-      val client = new CompositeApiClient(List(api1, api2))
 
-      client.send(payload) must beEqualTo(Left(TotalApiError(List(error1, error2)))).await
+      api1.clientId returns "api1"
+      api1.send(any[NotificationPayload])(any[ExecutionContext]) returns Future(Left(error1))
+      api2.clientId returns "api2"
+      api2.send(any[NotificationPayload])(any[ExecutionContext]) returns Future(Left(error2))
+      api3.clientId returns "api3"
+      api3.send(any[NotificationPayload])(any[ExecutionContext]) returns Future(Left(error3))
+      val client = new CompositeApiClient(List(api1, api2, api3))
+
+      val sourcedErrorList = List(ErrorWithSource("api1", error1), ErrorWithSource("api2", error2), ErrorWithSource("api3", error3) )
+
+      client.send(payload) must beEqualTo(Left(TotalApiError(sourcedErrorList))).await
 
     }
+
+    "Report partial error if some of the api calls fail" in {
+      val api1 = mock[ApiClient]
+      val api2 = mock[ApiClient]
+      val api3 = mock[ApiClient]
+
+      val error1 = HttpApiError(500)
+      val error3 = HttpApiError(403)
+
+      api1.send(any[NotificationPayload])(any[ExecutionContext]) returns Future(Left(error1))
+      api1.clientId returns "api1"
+      api2.send(any[NotificationPayload])(any[ExecutionContext]) returns Future(Right("{successful json response would go here}"))
+      api2.clientId returns "api2"
+      api3.send(any[NotificationPayload])(any[ExecutionContext]) returns Future(Left(error3))
+      api3.clientId returns "api3"
+
+      val client = new CompositeApiClient(List(api1, api2, api3))
+
+      val sourcedErrorList = List(ErrorWithSource("api1", error1), ErrorWithSource("api3", error3) )
+      client.send(payload) must beEqualTo(Left(PartialApiError(sourcedErrorList))).await
+
+
+    }
+
+    "Return first api response on success" in {
+      val api1 = mock[ApiClient]
+      val api2 = mock[ApiClient]
+      val api3 = mock[ApiClient]
+
+      api1.send(any[NotificationPayload])(any[ExecutionContext]) returns  Future(Right("Api_1_response"))
+      api2.send(any[NotificationPayload])(any[ExecutionContext]) returns Future(Right("Api_2_response"))
+      api3.send(any[NotificationPayload])(any[ExecutionContext]) returns  Future(Right("Api_3_response"))
+
+      val client = new CompositeApiClient(List(api1, api2, api3))
+
+      client.send(payload) must beEqualTo(Right("Api_1_response")).await
+    }
+
   }
-
-
 
 }
